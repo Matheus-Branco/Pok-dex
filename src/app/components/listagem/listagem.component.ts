dc393a819df71d8d3d228bc58ba231a60e554d75
Matheus-Branco <jeremiasmatheus007@gmail.com>
2024-09-27 09:22:45 -0300

    Implementando Busca com output e rota detalhes

---
+++ src/app/components/listagem/listagem.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../../models/pokemon';
+import { CoresBackgroundTipo } from '../../models/cores-background-tipo';
+import { PokeApiService } from '../../services/poke-api.service';
+import { converterParaTitleCase } from '../../util/converter-para-title-case';
+import { NgClass, NgForOf, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { mapearTipoPokemon } from '../../util/mapear-tipo-pokemon';
+import { CardPokemonComponent } from './card-pokemon/card-pokemon.component';
+import { BuscaComponent } from '../busca/busca.component';
+
+@Component({
+  selector: 'app-listagem',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgClass,
+    NgIf,
+    RouterLink,
+    CardPokemonComponent,
+    BuscaComponent,
+  ],
+  templateUrl: './listagem.component.html',
+})
+export class ListagemComponent implements OnInit {
+  public pokemons: Pokemon[];
+
+  public buscaRealizada: boolean = false;
+
+  private offsetPaginacao: number;
+
+  constructor(private pokeApiService: PokeApiService) {
+    this.pokemons = [];
+    this.offsetPaginacao = 0;
+  }
+
+  public ngOnInit(): void {
+    this.obterPokemons();
+  }
+
+  public buscarMaisResultados(): void {
+    this.offsetPaginacao += 20;
+
+    this.obterPokemons();
+  }
+
+  public filtrarPokemons(textoFiltro: string): void {
+    this.buscaRealizada = true;
+
+    this.pokemons = this.pokemons.filter((p) => {
+      return p.nome.toLowerCase().includes(textoFiltro.toLowerCase());
+    });
+  }
+
+  public limparFiltro() {
+    this.buscaRealizada = false;
+
+    this.pokemons = [];
+    this.obterPokemons();
+  }
+
+  private obterPokemons() {
+    this.pokeApiService
+      .selecionarTodos(this.offsetPaginacao)
+      .subscribe((res) => {
+        const arrayResultados = res.results as any[];
+
+        for (let resultado of arrayResultados) {
+          this.pokeApiService
+            .selecionarDetalhesPorUrl(resultado.url)
+            .subscribe((objDetalhes: any) => {
+              const pokemon = this.mapearPokemon(objDetalhes);
+
+              this.pokemons.push(pokemon);
+            });
+        }
+
+        this.pokemons.sort((p) => p.id);
+      });
+  }
+
+  private mapearPokemon(obj: any): Pokemon {
+    return {
+      id: obj.id,
+      nome: converterParaTitleCase(obj.name),
+      urlSprite: obj.sprites.other.dream_world.front_default,
+      tipos: obj.types.map(mapearTipoPokemon),
+    };
+  }
+}
---
+++ src/app/models/detalhes-pokemon.ts
@@ -0,0 +1,10 @@
+import { TipoPokemon } from './tipo-pokemon';
+
+export interface DetalhesPokemon {
+  id: number;
+  nome: string;
+  sprites: string[];
+  tipos: TipoPokemon[];
+  altura: number;
+  peso: number;
+}
